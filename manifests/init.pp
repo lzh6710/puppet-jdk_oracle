3e57cddd01dd56013686dc8ed106f65db2607091
lzh6710 <lzh6710@users.noreply.github.com>
2014-10-09 00:54:55 +0800

    change wget timeout.

--- manifests/init.pp
+++ manifests/init.pp
@@ -138,8 +138,8 @@ class jdk_oracle(
       exec { 'get_jdk_installer':
         cwd     => $install_dir,
         creates => "${install_dir}/${installerFilename}",
-        command => "wget -c --no-cookies --no-check-certificate --header \"Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com\" --header \"Cookie: oraclelicense=accept-securebackup-cookie\" \"${javaDownloadURI}\" -O ${installerFilename}",
-        timeout => 600,
+        command => "wget -t 10 -w 60 -c --no-cookies --no-check-certificate --header \"Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com\" --header \"Cookie: oraclelicense=accept-securebackup-cookie\" \"${javaDownloadURI}\" -O ${installerFilename}",
+        timeout => 60000,
         require => Package['wget'],
       }
 
